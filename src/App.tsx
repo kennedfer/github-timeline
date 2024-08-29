d25b845f36beec49a316a7da57185927375df291
kennedfer <kenned14santos@gmail.com>
2024-08-29 08:20:00 -0300

    feat: basic ui struture with conditional render logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import './App.css'
-import UsernameInput from './components/username-input/username-input';
+import { useEffect, useState } from "react";
+import "./App.css";
+import UsernameInput from "./components/username-input/username-input";
+import TimelineContainer from "./components/timeline/timeline-container/timeline-container";
 
+const App = () => {
+  const [githubUserInfo, setGithubUserInfo] = useState({});
+  const [hasGithubInfo, setHasGithubInfo]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState(false);
 
-const App  = () => {
-  console.log("teste");
+  useEffect(() => {
+    if (Object.keys(githubUserInfo).length > 0) setHasGithubInfo(true);
+  }, [githubUserInfo]);
 
+  return (
+    <>
+      <div>
+        <UsernameInput callback={setGithubUserInfo} />
+        {hasGithubInfo && ( //conditional render
+          <div>
+            <h1>{githubUserInfo.name}</h1>
+            <i>{githubUserInfo.bio}</i>
+            <TimelineContainer repositories={githubUserInfo.repositories} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-  return <>
-    <div>
-      <UsernameInput/>
-    </div>
-  </>
-}
-
-export default App
+export default App;
