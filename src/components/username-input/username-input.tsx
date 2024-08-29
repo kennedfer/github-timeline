6d5e1f12a9888ad068ac847a94e7bfcbb851d7ec
kennedfer <kenned14santos@gmail.com>
2024-08-29 08:22:53 -0300

    feat: controlled input of gh username, with some css style

--- src/components/username-input/username-input.tsx
+++ src/components/username-input/username-input.tsx
@@ -1,8 +1,28 @@
-const UsernameInput = () => {
-  return <>
-    <input type="text" />
-    <button>Buscar</button>
-  </>;
+import { useState } from "react";
+import { GithubUserInfo } from "../../types/github/github-timeline-api.types";
+import styles from './username-input.module.css'
+
+const UsernameInput = ({callback}: {callback: React.Dispatch<React.SetStateAction<GithubUserInfo>>}) => {
+  const [username, setUsername]:[string, React.Dispatch<React.SetStateAction<string>>] = useState("");
+
+  const handleUsernameChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(evt.target.value);
+  }
+
+  const searchuser = async (evt) => {
+    evt.target.disabled = true;
+    const data:Response = await fetch("http://localhost:3000/github/"+username);
+    const githubUserInfo = await data.json();
+
+    callback(githubUserInfo);
+    
+    evt.target.disabled = false;
+  }
+
+  return <div className={styles["input-container"]}>
+    <input className={styles.input} onChange={handleUsernameChange} type="text" />
+    <button value={username} onClick={searchuser}>Buscar</button>
+  </div>;
 }
 
 export default UsernameInput;
